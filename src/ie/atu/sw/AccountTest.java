8d5c6270f1bc9f724e0d4c718f1e065b711e0b10
sttadic <stjepan.tadich@gmail.com>
2024-12-07 22:34:32 +0000

    Implement parameterized test for getters

--- src/ie/atu/sw/AccountTest.java
+++ src/ie/atu/sw/AccountTest.java
@@ -7,83 +7,81 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.TestInfo;
 
 class AccountTest {
-	private Account acc;
+	private Account account;
 	
 	@BeforeEach
 	void setUp() {
-		this.acc = new Account("Stjepan", 1000);
+		this.account = new Account("Stjepan", 1000);
 	}
 
 	@Test
 	void testConstructor() {
-		assertEquals("Stjepan", acc.getAccountHolder());
-		assertEquals(1000, acc.getBalance());
-		assertEquals(0, acc.getLoan());
+		assertEquals("Stjepan", account.getAccountHolder());
+		assertEquals(1000, account.getBalance());
+		assertEquals(0, account.getLoan());
 	}
 	
-	@Test
-	void testGetAccountHolder() {
-		assertEquals("Stjepan", acc.getAccountHolder());
-	}
-	
-	@Test
-	void testGetBalance() {
-		assertEquals(1000, acc.getBalance());
-	}
-	
-	@Test
-	void testGetLoan() {
-		assertEquals(0, acc.getLoan());
+	@ParameterizedTest
+	@CsvSource({"John, 2000, 0", "Ann, 500, 200"})
+	void testAccountGetters(String accountHolder, double balance, double loan) {
+		Account acc = new Account(accountHolder, balance);
+		acc.approveLoan(loan);
+		assertEquals(accountHolder, acc.getAccountHolder());
+		assertEquals(balance, acc.getBalance());
+		assertEquals(loan, acc.getLoan());
 	}
 	
 	@Test
 	void testDeposit() {
-		acc.deposit(500);
-		assertEquals(1500, acc.getBalance());
+		account.deposit(500);
+		assertEquals(1500, account.getBalance());
 	}
 	
 	@Test
 	void testWithdrawSuccess() {
-		boolean result = acc.withdraw(200);
+		boolean result = account.withdraw(200);
 		assertTrue(result);
-		assertEquals(800, acc.getBalance());
+		assertEquals(800, account.getBalance());
 	}
 	
 	@Test
 	void testWithdrawInsufficientFunds() {
-		boolean result = acc.withdraw(1200);
+		boolean result = account.withdraw(1200);
 		assertFalse(result);
-		assertEquals(1000, acc.getBalance());
+		assertEquals(1000, account.getBalance());
 	}
 	
 	@Test
 	void testApproveLoan() {
-		acc.approveLoan(500);
-		assertEquals(500, acc.getLoan());
+		account.approveLoan(500);
+		assertEquals(500, account.getLoan());
 	}
 
 	@Test
 	void testRepayLoanSuccess() {
-		acc.approveLoan(500);
-		boolean result = acc.repayLoan(300);
+		account.approveLoan(500);
+		boolean result = account.repayLoan(300);
 		assertTrue(result);
-		assertEquals(200, acc.getLoan());
+		assertEquals(200, account.getLoan());
 	}
 	
 	@Test
 	void testRepayLoanFail() {
-		acc.approveLoan(500);
-		boolean result = acc.repayLoan(700);
+		account.approveLoan(500);
+		boolean result = account.repayLoan(700);
 		assertFalse(result);
-		assertEquals(500, acc.getLoan());
-		assertEquals(1000, acc.getBalance());
+		assertEquals(500, account.getLoan());
+		assertEquals(1000, account.getBalance());
 	}
 	
 	@AfterEach
-	void tearDown() {
-		System.out.println("Test completed");
+	void tearDown(TestInfo testInfo) {
+		System.out.println("Test completed - " + testInfo.getDisplayName());
 	}
 
 }
