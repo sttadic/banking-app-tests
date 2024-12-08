fbd4ec14af08b6ebdcb232b77988d5d6a1140b15
sttadic <stjepan.tadich@gmail.com>
2024-12-08 21:05:47 +0000

    Add unit tests for all methods in AccountManager

--- src/ie/atu/sw/AccountManagerTest.java
+++ src/ie/atu/sw/AccountManagerTest.java
@@ -6,13 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class AccountManagerTest {
@@ -20,25 +24,26 @@ class AccountManagerTest {
 	private static BankDeposits bankDeposits;
 
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		bankDeposits = new BankDeposits();
-		accountManager= new AccountManager(bankDeposits);
+	static void setUpBeforeClass() {
+		System.out.println("Start unit tests in AccountManagerTest suite");
 	}
 	
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		bankDeposits = new BankDeposits();
+		accountManager = new AccountManager(bankDeposits);
 		accountManager.addAccount("Alice", 1000);
 		accountManager.addAccount("Bob", 500);
 	}
 	
 	@AfterEach
-	void tearDown(TestInfo testInfo) throws Exception {
+	void tearDown(TestInfo testInfo) {
 		System.out.println("Test completed - " + testInfo.getDisplayName());
 	}
 
 	@AfterAll
-	static void tearDownAfterClass(TestInfo testInfo) throws Exception {
-		System.out.println("All tests completed in " + testInfo.getDisplayName());
+	static void tearDownAfterClass() {
+		System.out.println("All tests in AccountManagerTest completed");
 	}
 
 	
@@ -57,16 +62,34 @@ class AccountManagerTest {
 	}
 	
 	@Test
+	@Timeout(value = 1, unit=TimeUnit.MILLISECONDS)
 	void testDepositGetBalanceSuccess() {
-		assertTrue(accountManager.deposit("Alice", 1000));
-		assertEquals(2000, accountManager.getBalance("Alice"));
+		assertTrue(accountManager.deposit("Alice", Double.MAX_VALUE));
+		assertEquals(Double.MAX_VALUE, accountManager.getBalance("Alice"));
 	}
 	
 	@ParameterizedTest
-	@ValueSource(doubles = {-500.0d, 0d, -0.1d})
+	@ValueSource(doubles = {0, -0.1, -Double.MIN_VALUE})
 	void testDepositFail(double depAmount) {
 		assertFalse(accountManager.deposit("Alice", depAmount));
 	}
 	
+	@Test
+	void testWithdrawThrowsException() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			accountManager.withdraw("Alice", 0);
+		});
+	}
 	
+	@ParameterizedTest
+	@CsvSource({"Alice, 0.1", "Bob, 500"})
+	void testWithdrawSuccess(String accHolder, double witAmount) {
+		assertTrue(accountManager.withdraw(accHolder, witAmount));
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"Alice, 1001", "Bob, 500.01"})
+	void testWithdrawFail(String accHolder, double witAmount) {
+		assertFalse(accountManager.withdraw(accHolder, witAmount));
+	}
 }
