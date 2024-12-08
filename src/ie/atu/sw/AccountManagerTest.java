7cab11a01ed7c5a292e587097a2ce4161895262e
sttadic <stjepan.tadich@gmail.com>
2024-12-08 19:17:17 +0000

    Implement tests for addAccount and deposit methods in AccountManager. Update testFindAccountThrowsException to parameterized test. Add before each and after each annotations

--- src/ie/atu/sw/AccountManagerTest.java
+++ src/ie/atu/sw/AccountManagerTest.java
@@ -1,12 +1,19 @@
 package ie.atu.sw;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AccountManagerTest {
 	private static AccountManager accountManager;
@@ -17,22 +24,49 @@ class AccountManagerTest {
 		bankDeposits = new BankDeposits();
 		accountManager= new AccountManager(bankDeposits);
 	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		accountManager.addAccount("Alice", 1000);
+		accountManager.addAccount("Bob", 500);
+	}
+	
+	@AfterEach
+	void tearDown(TestInfo testInfo) throws Exception {
+		System.out.println("Test completed - " + testInfo.getDisplayName());
 	}
 
-	@Test
-	void testFindAccountThrowsException() {
+	@AfterAll
+	static void tearDownAfterClass(TestInfo testInfo) throws Exception {
+		System.out.println("All tests completed in " + testInfo.getDisplayName());
+	}
+
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Ann", "123", "null"})
+	void testFindAccountThrowsException(String accHolder) {
 		assertThrows(IllegalArgumentException.class, () -> {
-			accountManager.findAccount(null);
+			accountManager.findAccount(accHolder);
 		});
 	}
 	
 	@Test
-	void testFindAccountSuccess() {
-		accountManager.addAccount("John", 10000);
-		assertNotNull( accountManager.findAccount("John"));
+	void testFindAddAccountSuccess() {
+		accountManager.addAccount("Tim", 1500);
+		assertNotNull(accountManager.findAccount("Tim"));
 	}
-
+	
+	@Test
+	void testDepositGetBalanceSuccess() {
+		assertTrue(accountManager.deposit("Alice", 1000));
+		assertEquals(2000, accountManager.getBalance("Alice"));
+	}
+	
+	@ParameterizedTest
+	@ValueSource(doubles = {-500.0d, 0d, -0.1d})
+	void testDepositFail(double depAmount) {
+		assertFalse(accountManager.deposit("Alice", depAmount));
+	}
+	
+	
 }
