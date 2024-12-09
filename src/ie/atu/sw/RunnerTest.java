4e06d04237aa972debd238efa39cff45dff25b2d
sttadic <stjepan.tadich@gmail.com>
2024-12-09 12:31:50 +0000

    Add RunnerTest suite for executing all test classes

---
+++ src/ie/atu/sw/RunnerTest.java
@@ -0,0 +1,15 @@
+package ie.atu.sw;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+	AccountTest.class,
+	AccountManagerTest.class,
+	LoanManagerTest.class,
+	BankDepositsTest.class
+})
+public class RunnerTest {
+
+}
